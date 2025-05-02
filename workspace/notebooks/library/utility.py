3ad29cc4b5dc077fe9b6ba61cbca45c0e8749108
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-05-02 11:13:40 -0500

    ok

--- workspace/notebooks/library/utility.py
+++ workspace/notebooks/library/utility.py
@@ -26,7 +26,45 @@ def load_csv_file():
     df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
     df.printSchema()
     df.show()
-
     write_parquet("base", df)
-    
+    spark.stop()
+
+def read_parquet(spark, file_name):
+    home_dir = os.getenv('HOME_DIR')
+    return spark.read.parquet(f"{home_dir}/parkets/{file_name}.parquet")
+
+def normalize():
+    home_dir = os.getenv('HOME_DIR')
+    spark = get_spark_context()
+    df = read_parquet(spark, "base")
+    df.printSchema()
+
+    df = df.drop("_c16")
+
+    # Renaming
+    name_map = {
+        "NOMBRE ": "names",
+        "Cómo se enteró de El Refugio?": "channel",
+        "RESERVA POR BOOKING.COM comisión 14% (SI/NO)": "booking_fee",
+        "No. DE PERSONAS & Manillas":"pax",
+        "FECHA DE LLEGADA o CONSUMO":"date_start",
+        "FECHA DE SALIDA":"date_end",
+        "No. DE NOCHES":"nights",
+        "TOTAL A PAGAR EN HOSPEDAJE / PASADIA":"price",
+        " PAGÓ RESERVA 50% O ABONO (si/no)":"half_deposit",# Use intenger
+        "UBICACIÓN":"room",
+        "CONSUMO ":"meals",
+        " TOTAL CONSUMO":"meal_price",
+        " PAGO EN ENFECTIVO":"pay_cash",
+        " PAGO EN DIGITAL":"pay_online",
+        "OBSERVACIONES / PROPINAS":"tip_notes",
+        "EGRESOS":"expenses",
+    }
+    for key in name_map:
+        df = df.withColumnRenamed(key, name_map[key])
+
+    df.printSchema()
+
+    write_parquet("base_renamed", df)
+
     spark.stop()
